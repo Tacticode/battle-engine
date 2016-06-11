8f70377ef4346e6e32a47e5f2146afd66ecb988a
hengruicao <hengruicao@gmail.com>
2016-06-11 14:16:44 +0800

    [Execute script]Only execute onTurn function now

--- src/tacticode/script/CharacterScript.cpp
+++ src/tacticode/script/CharacterScript.cpp
@@ -17,11 +17,22 @@ namespace script{
 		std::cerr << "set script" << std::endl;
 		_context->get()->Enter();
 		_script = std::make_shared<Script>(*_context, script);		
+		_script->run(_context);
 		_context->get()->Exit();
 	}
 	void CharacterScript::run(){
 		_context->get()->Enter();
-		_script->run(_context);
+		v8::Handle<v8::Object> global = _context->get()->Global();
+		v8::Handle<v8::Value> value = global->Get(v8String::fromString("onTurn")); 
+
+		if (value->IsFunction()) {
+		    v8::Handle<v8::Function> func = v8::Handle<v8::Function>::Cast(value);
+		    v8::Handle<v8::Value> args[2];
+
+		    v8::Handle<v8::Value> js_result = func->Call(global, 0, args);
+		} else {
+			std::cerr << "onTurn undefined" << std::endl;
+		}
 		_context->get()->Exit();
 	}
 
