d6add4d354cee6ce7572bc2991f70f65b81fbc60
Wilko <perruchon.wilko.isart@gmail.com>
2016-06-17 12:45:49 +0200

    LineAoe added

---
+++ src/tacticode/spell/LineAoeEffect.cpp
@@ -0,0 +1,28 @@
+#include "LineAoeEffect.hpp"
+
+namespace tacticode
+{
+	namespace spell
+	{
+		LineAoeEffect::LineAoeEffect(size_t aoe)
+			: m_aoe(aoe)
+		{
+		}
+//add context as parameter
+		void	LineAoeEffect::applyEffect(std::shared_ptr<engine::Character> caster, std::shared_ptr<engine::Cell> cell, Spell spell)
+		{
+			int los = 1;
+			if (los == true)
+			{
+				std::list<std::shared_ptr<engine::Character> > targets;
+				for (size_t i = 0; i < m_aoe; ++i)
+				{
+					if (cell->getCharacter())
+
+				}
+				// if (target)
+				//	target->applyDamage(spell.getPower() * caster->m_currentAttributes->power / target->m_currentAttributes->resilience);
+			}
+		}
+	}
+}
