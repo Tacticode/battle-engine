adcd95e26dbc2fd3bf05219ec2d140dd753bc0e7
Wilko <perruchon.wilko.isart@gmail.com>
2016-10-08 16:35:20 +0200

    Sort de zone en ligne

--- src/tacticode/spell/LineAoeEffect.cpp
+++ src/tacticode/spell/LineAoeEffect.cpp
@@ -11,10 +11,10 @@ namespace tacticode
 		}
 		void	LineAoeEffect::applyEffect(std::shared_ptr<engine::Character> caster, std::shared_ptr<engine::Cell> cell, engine::BattleEngine &engine, Spell spell, int isSecondary)
 		{
-			if ((engine.getMap()->hasCellLineOfSightOnCell(caster->getPosition().x, caster->getPosition().y, cell->getX(), cell->getY()) &&
-				(caster->getPosition().x == cell->getX() ||
-					caster->getPosition().y == cell->getY())) || isSecondary != -1)
-			{
+			// if ((engine.getMap()->hasCellLineOfSightOnCell(caster->getPosition().x, caster->getPosition().y, cell->getX(), cell->getY()) &&
+			//	(caster->getPosition().x == cell->getX() ||
+			//		caster->getPosition().y == cell->getY())) || isSecondary != -1)
+			// {
 				std::list<std::shared_ptr<engine::Cell> > targets;
 				engine::Vector2i d;
 				std::shared_ptr<engine::Map> map = engine.getMap();
@@ -26,7 +26,7 @@ namespace tacticode
 					d.y = caster->getPosition().y - cell->getY() > 0 ? -1 : 1;
 				for (size_t i = 0; i < m_aoe; ++i)
 				{
-					if (engine.getCharacter(map->getCell(cell->getX() + (i * d.x), cell->getY() + (i * d.y)).getCharacterId()))
+					if (map->isCellOnMap(cell->getX() + (i * d.x), cell->getY() + (i * d.y)) && engine.getCharacter(map->getCell(cell->getX() + (i * d.x), cell->getY() + (i * d.y)).getCharacterId()))
 						targets.push_back(std::make_shared<engine::Cell>(map->getCell(cell->getX() + (i * d.x), cell->getY() + (i * d.y))));
 				}
 
@@ -35,7 +35,7 @@ namespace tacticode
 					for (std::list<std::shared_ptr<engine::Cell>>::iterator it2 = targets.begin(); it2 != targets.end(); ++it2)
 						(*it)->applyEffect(caster, *it2, engine, spell);
 				}
-			}
+				//}
 		}
 	}
 }
