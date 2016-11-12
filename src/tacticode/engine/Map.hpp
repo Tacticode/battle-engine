943a7b43d367a2213a31b058852a8dc623c445e4
Jean Fauquenot <jean.fauquenot@gmail.com>
2016-11-12 14:02:48 +0100

    Add missing change in Map.hpp

--- src/tacticode/engine/Map.hpp
+++ src/tacticode/engine/Map.hpp
@@ -31,7 +31,7 @@ namespace tacticode
 			void deserialize(const file::IValue& json);
 
 			int getOctant(int originX, int originY, int targetX, int targetY) const;
-			bool hasLineOfSightByOctant(int originX, int originY, int targetX, int targetY, int octant, int originHeight) const;
+			bool hasLineOfSightByOctant(int x, int y, int octant, int originHeight) const;
 
 		public:
 			static const int fieldOfViewHeightLimit;
